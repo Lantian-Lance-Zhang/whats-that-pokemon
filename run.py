27291a47b86caa08d10062f70525c96e2dd9d7ed
LantianZhang39 <lancezhang0326@163.com>
2019-09-07 14:16:42 -0400

    updates

--- run.py
+++ run.py
@@ -1,95 +1,69 @@
 from flask import Flask, render_template, request, redirect
-import os
+import os, shelve
 from time import sleep
-TENSOR = True
-if TENSOR:
-    import tensorflow as tf
+import tensorflow as tf
 from PIL import Image
 import numpy as np
 from datetime import datetime
-import shelve
 from glob import glob
-
+from random import randrange
+from glob import glob
 app = Flask(__name__)
-model_id = "default"
-
+file_path = "default"
 @app.route("/")
 @app.route("/home")
 def home():
 	return render_template("home.html")
-
-@app.route("/options", methods=["GET", "POST"])
-def options():
-    return render_template("options.html")
-
-@app.route("/choose_file", methods=["GET", "POST"])
-def choose_file():
-    global model_id
-
-    if len(model_id) > 0:
-        model_id = request.form["model_id"]
-        # print([i.split("/")[-1][:-3] for i in glob("static/models/*.h5")])
-        # print(model_id)
-        if model_id not in [i.split("/")[-1][:-3] for i in glob("static/models/*.h5")]:
-            return render_template("options.html", warning=True)
-    return render_template("upload_file.html", model_id=model_id)
-
-@app.route("/skip")
-def skip():
-    return render_template("upload_file.html", model_id="18cat_93")
-
 @app.route("/before_upload")
 def before_upload():
     return render_template("before_upload.html")
-
-@app.route("/upload/<model_id>", methods=["GET", "POST"])
-def upload(model_id):
-    upload = request.files["inputFile"]
-    filename = "input" + str(datetime.now()) + ".png"
-    model_name = model_id + ".h5"
-    image_path = os.path.join("static/uploads", filename)
-    base = 128
-
-    if upload.filename.split(".")[-1] in ["png", "jpg"]:
-        if model_id == "I.h5":
-            prediction = "Ivysaur"
-            return render_template("result.html", result=prediction, filename=filename)
-
-        upload.save(image_path)
-        print("Processing Image: " + filename)
-        image = Image.open(image_path)
-        image = image.resize((base,base))
-        image.save(image_path)
-        image_array = np.array([np.asarray(image, dtype="float64")[:,:,:3] / 255.]).reshape((-1,base,base,3))
-        print("Image Processing Complete")
-
-        print("Loading model: " + model_name)
-        model = tf.keras.models.load_model(os.path.join("static/models", model_name))
-        print("Loading Complete")
-
-        prediction = model.predict(image_array)
-        if model_id == "fairy":
-            prediction = "Clefable" if prediction > 0.5 else "Clefairy"
-        else:
-            db = shelve.open("static/dictionaries")
-            prediction = list(prediction.flatten()).index(prediction.flatten().max())
-            print(list(db.keys()))
-            prediction = db[str(model_id.title())][prediction]
-
-        print("Prediction: " + str(prediction))
-        import random
-        target_dir = os.path.join("static/uploaded", prediction.title())
-        while True:
-            file = os.listdir(target_dir)[int(random.randrange(0, 10))]
-            if file[:1] != ".":
-                break
-        example = os.path.join("http://127.0.0.1:5000/static/uploaded/" + prediction.title(), file)#int(random.randrange(0, 10))])
-        print(example)
-
-        return render_template("result.html", result=prediction, filename=filename, example=example)
-
+@app.route("/options", methods=["GET","POST"])
+def options():
+    global file_path
+    file = request.files["inputFile"]
+    filename = file.filename
+    if filename.split(".")[-1] in ["png","jpg"]:
+        new_fn = "input" + str(datetime.now()) + ".png"
+        new_path = os.path.join("static/uploads", new_fn)
+        file.save(new_path) #can use some improvements here
+        file_path = new_path
     else:
         return render_template("upload_file.html", warning=True)
-
+    return render_template("options.html")
+@app.route("/upload_file", methods=["GET","POST"])
+def upload_file():
+    return render_template("upload_file.html")
+@app.route("/upload", methods=["GET","POST"])
+def upload():
+    global file_path
+    model_id = request.form["model_id"]
+    if model_id not in [i.split("/")[-1][:-3] for i in glob("static/models/*.h5")]:
+            return render_template("options.html", warning=True)
+    if len(model_id) > 0:
+        model_name = model_id + ".h5"
+    else:
+        model_id = "default"
+    image = Image.open(file_path)
+    image = image.resize((128,128))
+    image_array = np.array([np.asarray(image, dtype="float64")[:,:,:3] / 255.]).reshape((-1,128,128,3))
+    model = tf.keras.models.load_model(os.path.join("static/models",model_name))
+    prediction = list(model.predict(image_array)[0])
+    model_keys = shelve.open("static/databases/dictionaries")
+    prediction = prediction.index(max(prediction))
+    if model_id == "default":
+        prediction = "default"
+    elif len(model_id) > 1:
+        prediction = model_keys[model_id.lower()][prediction]
+    else:
+        prediction = model_keys[model_id.upper()][prediction]
+    target_dir = os.path.join("static/train", prediction.title())
+    sample_dir = glob(os.path.join(target_dir,"*.jpg"))[randrange(0, 25)]
+    sample = Image.open(sample_dir)
+    sample = sample.resize((500,500))
+    sample.save(sample_dir)
+    result_path = os.path.join("static/results","{} {} {}.png".format(prediction, model_id, str(datetime.now())))
+    image.save(result_path)
+    return render_template("result.html", result=prediction, file_path=result_path, example=sample_dir)
 if __name__ == "__main__":
+    print("Initializing...")
     app.run(debug=True)
---
+++ runner.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, request, redirect
+import os
+from time import sleep
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+from datetime import datetime
+import shelve
+from glob import glob
+from random import randrange
+from glob import glob
+
+app = Flask(__name__)
+file_path = "default"
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template("home.html")
+
+@app.route("/before_upload")
+def before_upload():
+    return render_template("before_upload.html")
+
+@app.route("/options", methods=["GET","POST"])
+def options():
+    global file_path
+
+    file = request.files["inputFile"]
+    filename = file.filename
+    print("Received file --> " + filename)
+
+    if filename.split(".")[-1] in ["png","jpg"]:
+        new_fn = "input" + str(datetime.now()) + ".png"
+        new_path = os.path.join("static/uploads", new_fn)
+        print("Assigned path --> " + new_path)
+        file.save(new_path) #can use some improvements here
+        file_path = new_path
+    else:
+        print("False data type --> " + filename.split(".")[-1])
+        return render_template("upload_file.html", warning=True)
+    return render_template("options.html")
+
+@app.route("/upload_file", methods=["GET","POST"])
+def upload_file():
+    return render_template("upload_file.html")
+
+@app.route("/upload", methods=["GET","POST"])
+def upload():
+    global file_path
+
+    #requesting model_id from options.html
+    model_id = request.form["model_id"]
+    if model_id.lower() not in [i.split("/")[-1][:-3] for i in glob("static/models/*.h5")]:
+        if model_id.lower() != "default":
+            return render_template("options.html", warning=True)
+        else:
+            model_id = "default"
+
+    #creating model_name according to model_id
+    if len(model_id) > 0:
+        print("Received model_id --> " + model_id + "\nLength = " + str(len(model_id)))
+        model_name = model_id + ".h5"
+    else:
+        print("Received model_id --> DEFAULT")
+        model_id = "default"
+
+    #loading and resizing a image
+    print("Loading image from " + file_path)
+    image = Image.open(file_path)
+    image = image.resize((128,128))
+    image_array = np.array([np.asarray(image, dtype="float64")[:,:,:3] / 255.]).reshape((-1,128,128,3))
+    print("Processing complete\nLoading model --> " + model_name)
+
+    #loading a model and making a prediction
+    model = tf.keras.models.load_model(os.path.join("static/models",model_name))
+    prediction = list(model.predict(image_array)[0])
+
+    #interpreting the prediction based on model_id
+    model_keys = shelve.open("static/databases/dictionaries")
+    prediction = prediction.index(max(prediction))
+    
+    if model_id == "default":
+        prediction = "default"
+    elif len(model_id) > 1:
+        prediction = model_keys[model_id.lower()][prediction]
+    else:
+        prediction = model_keys[model_id.upper()][prediction]
+    print("Prediction --> " + prediction)
+
+    #choosing a sample image of the pokemon & processing it
+    target_dir = os.path.join("static/train", prediction.title())
+    sample_dir = glob(os.path.join(target_dir,"*.jpg"))[randrange(0, 25)]
+    sample = Image.open(sample_dir)
+    sample = sample.resize((500,500))
+    sample.save(sample_dir)
+    print("Sample image --> " + sample_dir)
+
+    #saving data for future analysis
+    result_path = os.path.join("static/results","{} {} {}.png".format(prediction, model_id, str(datetime.now())))
+    print("Saving image to --> " + result_path)
+    image.save(result_path)
+
+    return render_template("result.html", result=prediction, file_path=result_path, example=sample_dir)
+
+if __name__ == "__main__":
+    print("Initializing...")
+    app.run(debug=True)
